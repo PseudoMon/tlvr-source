b17b0dfa8cd4cf9d8f8806ca3332930bfea18761
PseudoMon <aliya.nisa@yahoo.com>
2023-09-16 22:09:24 +0700

    Updated script to get voice lines and voice actors

--- datascript/transformer.py
+++ datascript/transformer.py
@@ -79,13 +79,18 @@ def download_avatars():
 		if count > 150:
 			break
 
-def grab_welcome():
-	table = {}
+def load_wordtables():
+	tables = {}
 	for lang in langs:
-		table[lang] = load_wordtable(lang)
+		tables[lang] = load_wordtable(lang)
 		print(f"Word table for {lang} loaded")
 		
 	print("Word tables loaded.")
+
+	return tables
+
+def get_welcome():
+	tables = load_wordtables()
 
 	key = "char_002_amiya_CN_042"
 	result = {}
@@ -96,4 +101,98 @@ def grab_welcome():
 	print("Texts in key found. Now saving...")
 	save_json(result, "welcome.json")
 
-grab_welcome()
+def get_voices(wordkey, wordtables):
+	# Use EN as base
+	basetable = wordtables["en"]
+
+	voices = []
+
+	for wordid in basetable["charWords"]:
+		basedata = basetable["charWords"][wordid]
+		if basedata["wordKey"] != wordkey:
+			continue
+
+		voicedata = {
+			"id": wordid,
+			"title": {"en": basedata["voiceTitle"]},
+			"text": {"en": basedata["voiceText"]},
+			"asset": basedata["voiceAsset"],
+		}
+
+		for lang in langs:
+			if lang == "en":
+				# Skip en
+				continue
+
+			langbasedata = wordtables[lang]["charWords"][wordid]
+			voicedata["title"][lang] = langbasedata["voiceTitle"]
+			voicedata["text"][lang] = langbasedata["voiceText"]
+
+		voices.append(voicedata)
+
+	return voices
+
+def get_actors_from_voicedict(voicedict):
+	# Might add other voice languages later
+	lang_mapping = {
+		"en": "EN",
+		"cn" :"CN_MANDARIN",
+		"jp": "JP",
+		"kr": "KR",
+	}
+
+	actors = {}
+	for lang in lang_mapping:
+		try:
+			actordata = voicedict[lang_mapping[lang]]
+			actors[lang] = actordata["cvName"]
+		except KeyError:
+			# No actor data for this language
+			print(f"No actor found for language {lang}")
+			pass
+
+	return actors
+
+def get_actors(charid, wordtables):
+	# JP use the native spelling of the name
+	# EN use English preferred name or transliteration
+	# CN use CN names even for Korean and Japanese names with kana
+	# So we use JP as base
+	basetable = wordtables["jp"]
+	voidedict = basetable["voiceLangDict"][charid]["dict"]
+	native_names = get_actors_from_voicedict(voidedict)
+
+	# Add EN names as they're often the global preferred name
+	entable = wordtables["en"]
+	envoicedict = entable["voiceLangDict"][charid]["dict"]
+	global_names = get_actors_from_voicedict(envoicedict)
+
+	actors = {}
+	for lang in native_names:
+		actors[lang] = {}
+		actors[lang]["native"] = native_names[lang]
+		actors[lang]["global"] = global_names[lang]
+
+	return actors
+
+def check_voice_availability(charid, wordtables):
+	# Check which languages this character has voice of
+	# Check in CN since it might not be on global yet
+
+	cntable = wordtables["cn"]
+	envoicedict = basetable["voiceLangDict"][charid]["dict"]
+	cn_names = get_actors_from_voicedict(voidedict)
+	# technically the name itself is not needed
+	# but im too lazy to make another function
+
+	availability = []
+	for lang in cn_names:
+		availability.append(lang)
+
+	return availability
+
+wordtables = load_wordtables()
+#wordkey = f"char_{listdata["numberid"]}_{listdata["wordid"]}"
+wordkey = "char_469_indigo"
+texas_actors = get_actors(wordkey, wordtables)
+print(texas_actors)
