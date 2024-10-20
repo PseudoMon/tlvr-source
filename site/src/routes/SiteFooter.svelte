d0d118e3f887b3c4040f10a0c5170fb11a4991ea
PseudoMon <aliya.nisa@yahoo.com>
2024-10-20 13:35:58 +0700

    Updated data up to Babel

--- site/src/routes/SiteFooter.svelte
+++ site/src/routes/SiteFooter.svelte
@@ -5,7 +5,7 @@
 <footer>
   <a href="https://github.com/PseudoMon/tlvr-source" target="_blank"><GithubIcon /></a>
   <span>Site version 1.2.1</span>
-  <span>Data last updated: 2024-09-15 (Operation Lucent Arrowhead on Global)</span>
+  <span>Data last updated: 2024-10-20 (Babel on Global)</span>
 </footer>
 
 <style>
