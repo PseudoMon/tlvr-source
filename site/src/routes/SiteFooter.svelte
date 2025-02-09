b087758a288dcdde4c0033254c9e9454b51060ce
PseudoMon <aliya.nisa@yahoo.com>
2025-02-09 14:27:58 +0700

    Updated data up to A Kazdelian Rescue

--- site/src/routes/SiteFooter.svelte
+++ site/src/routes/SiteFooter.svelte
@@ -5,7 +5,7 @@
 <footer>
   <a href="https://github.com/PseudoMon/tlvr-source" target="_blank"><GithubIcon /></a>
   <span>Site version 1.3</span>
-  <span>Data last updated: 2025-01-28 (Inudi Harek, Horakhet on Global)</span>
+  <span>Data last updated: 2025-02-09 (A Kazdelian Rescue on Global)</span>
 </footer>
 
 <style>
